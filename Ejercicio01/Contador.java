e5a078b05a64dd5b9a4af57dd313b565944b6162
Sebastian Adrian Guzman Gomez <sg8129758@gmail.com>
2026-02-11 02:40:04 +0000

    Feb 11, 2026, 2:40 AM

--- Ejercicio01/Contador.java
+++ Ejercicio01/Contador.java
@@ -1,6 +1,6 @@
 public class Contador {
     public static void main(String[] args) {
         System.out.println("Estoy programando desde una tablet 😎");
-        System.out.println("");
+        System.out.println("Holaaa");
     }
 }
