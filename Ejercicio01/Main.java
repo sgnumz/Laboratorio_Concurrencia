8a043a1938db6bc7d185b7e2700cfa2dc2ebfd6d
Sebastian Adrian Guzman Gomez <sg8129758@gmail.com>
2026-02-11 02:47:55 +0000

    Feb 11, 2026, 2:47 AM

--- Ejercicio01/Main.java
+++ Ejercicio01/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Estoy programando desde una tablet 😎");
         System.out.println("Holaaa");
+        System.out.println("Adioss");
     }
 }
